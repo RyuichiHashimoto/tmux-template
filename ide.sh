6c08db84826915d8370975262e5f1fba5d456974
RyuichiHashimoto <ryuichi.hashimoto.git@gmail.com>
2023-04-23 17:27:00 +0900

    add comment

--- ide.sh
+++ ide.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
-set -o errexit
-set -o nounset
-set -o pipefail
+
+set -o errexit  # Exit script if any command fails
+set -o nounset  # Exit script if undefined variable is used
+set -o pipefail # Exit script if any command in pipeline fails
 
 
+# Split pane horizontally
 tmux split-window -h
+
+
+
 tmux select-pane -t 1
 tmux split-window -v
 tmux split-window -v
-tmux select-layout main-vertical
+tmux select-layout main-vertical  # Arrange panes vertically
 
 tmux select-pane -t 0
-tmux resize-pane -R 25
-
+tmux resize-pane -R 25  # Resize current pane to the right
 
 tmux split-window -v
-tmux resize-pane -D 15
+tmux resize-pane -D 15  # Resize current pane downwards
+tmux select-pane -t 0  # Select first pane
 
-tmux select-pane -t 0
